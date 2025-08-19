1aac041b4a92cc5aff934edfd696c3227d21ca34
Daniel <danielpapereiraJV@gmail.com>
2025-08-18 21:58:48 -0300

    feat/mark created - Componente de marcas Criado !

---
+++ econverse-app/src/feature/Mark/model/items-mark.ts
@@ -0,0 +1,7 @@
+export const itemsMark = [
+  { id: 1, src: '../../../../public/econverse-logo-header.svg', alt: 'Logo Econverse' },
+  { id: 2, src: '../../../../public/econverse-logo-header.svg', alt: 'Logo Econverse' },
+  { id: 3, src: '../../../../public/econverse-logo-header.svg', alt: 'Logo Econverse' },
+  { id: 4, src: '../../../../public/econverse-logo-header.svg', alt: 'Logo Econverse' },
+  { id: 5, src: '../../../../public/econverse-logo-header.svg', alt: 'Logo Econverse' }
+];
